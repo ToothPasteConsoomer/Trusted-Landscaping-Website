1d590c86ca6c5bcaf4ff409f6ba3ca219ec0e424
ToothPasteConsoomer <112597075+ToothPasteConsoomer@users.noreply.github.com>
2024-02-22 11:00:58 -0800

    Update script.js

--- script.js
+++ script.js
@@ -9,32 +9,30 @@ const cityInput = document.getElementById('city');
 const optionInput = document.getElementById('options');
 const formContact = document.getElementById('form-contact'); // Replace with the actual ID of your form
 
-formContact.addEventListener('submit', (e) => {
+formContact.addEventListener('submit', (e)=> {
     e.preventDefault();
-    
+    let ebody = `
+        <h2>Name: </h2>${firstNameInput.value}&nbsp;${lastNameInput.value}
+        <br>
+        <h2>Email: </h2>${emailInput.value}
+        <br>
+        <h2>City: </h2>${cityInput.value}
+        <br>
+        <h2>Phone: </h2>${mobileNumberInput.value}
+        <br>
+        <h2>Option: </h2>${optionInput.value}
+        <br>
+    `;
     if (!emailInput.value) {
         errorMessage.textContent = 'Please fill in all required fields *';
         return;
-    }
-
-    let ebody = `
-        <b>Name: </b>${firstNameInput.value}&nbsp;${lastNameInput.value}
-        <br>
-        <b>Email: </b>${emailInput.value}
-        <br>
-        <b>City: </b>${cityInput.value}
-        <br>
-        <b>Phone: </b>${mobileNumberInput.value}
-        <br>
-        <b>Option: </b>${optionInput.value}
-        <br>
-    `;
-
+    } else {
     successMessage.textContent = 'Quote sent successfully!';
+    }
     Email.send({
         SecureToken: "29b73984-0843-426d-8433-6602f28945ce",
         To: "moorearic5@gmail.com",
-        From: emailInput.value,
+        From: "moorearic5@gmail.com,
         Subject: "Quote Request",
         Body: ebody
     })
