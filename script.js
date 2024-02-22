ce49837ac646aee355c774799f88df5b9f27cb43
ToothPasteConsoomer <112597075+ToothPasteConsoomer@users.noreply.github.com>
2024-02-21 20:12:02 -0800

    Update script.js

--- script.js
+++ script.js
@@ -2,19 +2,15 @@ const users = [];
 
 const successMessage = document.getElementById('success-message');
 const errorMessage = document.getElementById('error-message');
-
+const firstNameInput = document.getElementById('fname').value;
+const lastNameInput = document.getElementById('lname').value;
+const mobileNumberInput = document.getElementById('phone').value;
+const emailInput = document.getElementById('email').value;
+const cityInput = document.getElementById('city').value;
+const optionInput = document.getElementById('options').value;
 submit.addEventListener('submit',(e)=>{
 	e.preventDefault();
 	console.log("Clicked");
-})	
-signupForm.addEventListener('submit', function(event) {
-	event.preventDefault();
-	const firstNameInput = document.getElementById('fname').value;
-	const lastNameInput = document.getElementById('lname').value;
-	const mobileNumberInput = document.getElementById('phone').value;
-	const emailInput = document.getElementById('email').value;
-	const cityInput = document.getElementById('city').value;
-	const optionInput = document.getElementById('options').value;
 
 	if (!emailInput) {
 		errorMessage.textContent = 'Please fill in all required fields *';
@@ -32,14 +28,7 @@ signupForm.addEventListener('submit', function(event) {
 		<b>Option: </b>${options.value}
 		<br>
 		'
-	users.push ({
-		firstName: firstNameInput,
-		lastName: lastNameInput,
-		phone: phoneInput,
-		email: emailInput,
-		city: cityInput,
-	});
-	successMessage.textContent = 'Quote sent sccuessfully!';
+    successMessage.textContent = 'Quote sent sccuessfully!';
     Email.send({
     SecureToken : "aa82427b-69d2-4421-b398-622278da0b54",
     To : "moorearic5@gmail.com",
